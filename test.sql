f544b1dbfbe991663faf59d1d7416ec78519943e
Muhammad Waqar <muhammad.9035@iqra.edu.pk>
2025-10-05 19:30:34 +0500

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,34 @@
+--Answer#1
+select top 5 c.customerid,c.Name as customer_name,o.totalamount
+from dbo.Customer c
+inner join 
+dbo.salesorder o
+on 
+c.customerid=o.customerid
+order by 
+o.totalamount desc
+;
+---Answer#2
+select   s.supplierid,s.name, count(d.ProductID) as product_Count
+from dbo.supplier s
+join
+dbo.purchaseorder p
+on
+s.supplierid=p.supplierid
+join
+dbo.purchaseorderdetail d
+on
+p.orderid=d.orderid
+group by  s.Name,
+			s.SupplierID
+			;
+
+
+
+
+
+
+
+
+
 
