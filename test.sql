196033a52852566d3e524422530d077a5a6de8c5
Muhammad Waqar <muhammad.9035@iqra.edu.pk>
2025-10-11 15:27:01 +0500

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,3 +1,6 @@
+-- Q1. List top 5 customers by total order amount.
+-- Retrieve the top 5 customers who have spent the most across all sales orders. Show CustomerID, CustomerName, and TotalSpent.
+
 --Answer#1
 select top 5 c.customerid,c.Name as customer_name,o.totalamount
 from dbo.Customer c
@@ -8,6 +11,10 @@ c.customerid=o.customerid
 order by 
 o.totalamount desc
 ;
+-- Q2. Find the number of products supplied by each supplier.
+-- Display SupplierID, SupplierName, and ProductCount. Only include suppliers that have more than 10 products.
+
+
 ---Answer#2
 select   s.supplierid,s.name, count(d.ProductID) as product_Count
 from dbo.supplier s
@@ -23,6 +30,10 @@ group by  s.Name,
 			s.SupplierID
 			;
 
+
+-- Q3. Identify products that have been ordered but never returned.
+-- Show ProductID, ProductName, and total order quantity.
+
 ---Ans#3
 SELECT 
     p.ProductID,
@@ -35,6 +46,10 @@ LEFT JOIN dbo.Returns R ON d.ReturnID = R.ReturnID
 WHERE R.ReturnID IS NULL
 GROUP BY p.ProductID, p.Name
 HAVING SUM(o.Quantity) > 0;
+
+
+-- Q4. For each category, find the most expensive product.
+-- Display CategoryID, CategoryName, ProductName, and Price. Use a subquery to get the max price per category.
 
 ---Ans#4
 
@@ -51,6 +66,11 @@ WHERE p.Price = (
     WHERE p2.CategoryID = c.CategoryID
 )
 ORDER BY c.CategoryID, p.Price DESC;
+
+
+-- Q5. List all sales orders with customer name, product name, category, and supplier.
+-- For each sales order, display:
+-- OrderID, CustomerName, ProductName, CategoryName, SupplierName, and Quantity.
 
 ---Ans#5
 select c.Name as customer_name,so.OrderID,p.Name as product_name,ct.Name as category_name,pdo.Quantity,s.Name as supplier_name
@@ -82,6 +102,73 @@ join
 dbo.Supplier s
 on s.SupplierID=po.SupplierID;
 
+-- Q6. Find all shipments with details of warehouse, manager, and products shipped.
+-- Display:
+-- ShipmentID, WarehouseName, ManagerName, ProductName, QuantityShipped, and TrackingNumber.
+---Ans#6
+SELECT 
+    s.ShipmentID,
+    -- w.Name as WarehouseName, # there is not find any data related to wharehouse name in the tables.
+    e.Name as ManagerName,
+    p.Name as ProductName,
+    sd.Quantity as QuantityShipped,
+    s.TrackingNumber
+FROM dbo.Shipment s
+JOIN dbo.Warehouse w ON s.WarehouseID = w.WarehouseID
+JOIN dbo.Employee e ON w.ManagerID = e.EmployeeID
+JOIN dbo.ShipmentDetail sd ON s.ShipmentID = sd.ShipmentID
+JOIN dbo.Product p ON sd.ProductID = p.ProductID
+ORDER BY s.ShipmentID;
+
+
+-- Q7. Find the top 3 highest-value orders per customer using RANK(). Display CustomerID, CustomerName, OrderID, and TotalAmount.
+
+---Ans#7
+WITH RankedOrders AS (
+    SELECT 
+        c.CustomerID,
+        c.Name AS CustomerName,
+      p.OrderID AS OrderID,
+        so.TotalAmount,
+        DENSE_RANK() OVER (
+            PARTITION BY c.CustomerID 
+            ORDER BY so.TotalAmount DESC
+        ) AS OrderRank
+    FROM dbo.Customer c
+    JOIN dbo.SalesOrder so ON c.CustomerID = so.CustomerID
+	join dbo.Payment p on so.OrderID=p.OrderID
+)
+SELECT 
+    CustomerID,
+    CustomerName,
+    OrderID,
+    TotalAmount
+FROM RankedOrders
+WHERE OrderRank <= 3
+ORDER BY CustomerID, OrderRank;
+
+
+-- Q8. For each product, show its sales history with the previous and next sales quantities (based on order date). Display ProductID, ProductName, OrderID, OrderDate, Quantity, PrevQuantity, and NextQuantity.
+---Answ#8
+SELECT 
+    p.ProductID,
+    p.Name AS ProductName,
+    py. OrderID as orderid,
+    so.OrderDate,
+    sod.Quantity,
+    LAG(sod.Quantity) OVER (
+        PARTITION BY p.ProductID 
+        ORDER BY so.OrderDate
+    ) AS PrevQuantity,
+    LEAD(sod.Quantity) OVER (
+        PARTITION BY p.ProductID 
+        ORDER BY so.OrderDate
+    ) AS NextQuantity
+FROM dbo.Product p
+JOIN dbo.SalesOrderDetail sod ON p.ProductID = sod.ProductID
+JOIN dbo.SalesOrder so ON sod.OrderID = so.OrderID
+JOIN DBO.Payment py on so.OrderID=py.orderid
+ORDER BY p.ProductID, so.OrderDate;
 
 
 
